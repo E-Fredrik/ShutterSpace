e921ceb3afcd001fe673ed88becf14d0ad7e2c8e
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2026-05-14 10:51:33 +0700

    Initial Commit

---
+++ ShutterSpace/ShutterSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterSpaceApp.swift
+//  ShutterSpace
+//
+//  Created by Elifele Fredrik on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ShutterSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
